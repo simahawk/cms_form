405d327c2a6df7d8dcbcc6f52a3e4d42f64ee199
Simone Orsi <simahawk@gmail.com>
2016-12-11 13:17:45 +0100

    fields loaders and init overrides

--- cms_form/models/cms_form.py
+++ cms_form/models/cms_form.py
@@ -4,6 +4,12 @@
 
 from openerp import models
 from openerp import fields
+from openerp import _
+from openerp.http import request
+
+import json
+import werkzeug.urls
+import inspect
 
 
 IGNORED_FORM_FIELDS = (
@@ -11,6 +17,49 @@ IGNORED_FORM_FIELDS = (
     'display_name',
     'id',
 )
+
+
+def m2o_to_form(item, value, **req_values):
+    if isinstance(value, basestring) and value.isdigit():
+        # number as string
+        return int(value)
+    elif isinstance(value, models.BaseModel):
+        return value and value.id or None
+    return None
+
+
+def x2many_to_form(item, value, display_field='display_name', **req_values):
+    value = [{'id': x.id, 'name': x[display_field]} for x in value]
+    value = json.dumps(value)
+    return value
+
+
+def form_to_integer(item, value, **req_values):
+    try:
+        return int(value)
+    except ValueError:
+        return 0.0
+
+
+def form_to_float(item, value, **req_values):
+    try:
+        return float(value)
+    except ValueError:
+        return 0.0
+
+DEFAULT_LOADERS = {
+    # important: return False if no value
+    # otherwise you will compare an empty recordset with an id
+    # in a select input in form widget template.
+    'many2one': m2o_to_form,
+    'one2many': x2many_to_form,
+    'many2many': x2many_to_form,
+}
+DEFAULT_EXTRACTORS = {
+    'integer': form_to_integer,
+    'many2one': form_to_integer,
+    'float': form_to_float,
+}
 
 
 class CMSForm(models.AbstractModel):
@@ -23,11 +72,27 @@ class CMSForm(models.AbstractModel):
     # model's fields to load
     _form_model_fields = []
     _form_required_fields = ()
-    # TODO
-    form_control_fields = ()
-    _fields_attributes = [
+    _form_fields_attributes = [
         'type', 'string', 'domain', 'required', 'readonly',
     ]
+    _form_fields_whitelist = ()
+    _form_fields_blacklist = ()
+    _form_extractors = DEFAULT_EXTRACTORS
+    _form_loaders = DEFAULT_LOADERS
+
+    def form_init(self, request, **kw):
+        """Initalize a form.
+
+        @param request: an odoo-wrapped werkeug request
+        @parm kw: pass any override for `_form_` attributes
+            ie: `fields_attributes` -> `_form_fields_attributes`
+        """
+        self.o_request = request  # odoo wrapped request
+        self.request = request.httprequest  # werkzeug request
+        # override `_form_` parameters
+        for k, v in kw.iteritems():
+            if not inspect.ismethod(getattr(self, '_form_' + k)):
+                setattr(self, '_form_' + k, v)
 
     @property
     def form_model(self):
@@ -38,14 +103,23 @@ class CMSForm(models.AbstractModel):
         _all_fields = {}
         # load model fields
         _model_fields = self.form_model.fields_get(
-            self._form_model_fields, attributes=self._fields_attributes)
-        _form_fields = self.fields_get(attributes=self._fields_attributes)
+            self._form_model_fields, attributes=self._form_fields_attributes)
+        _form_fields = self.fields_get(attributes=self._form_fields_attributes)
         # remove unwanted fields
         for fname in IGNORED_FORM_FIELDS:
             _form_fields.pop(fname, None)
         _all_fields.update(_model_fields)
         # form fields override model fields
         _all_fields.update(_form_fields)
+        # exclude blacklisted
+        for fname in self._form_fields_blacklist:
+            # make it fail if passing wrong field name
+            _all_fields.pop(fname)
+        # include whitelisted
+        _all_whitelisted = {}
+        for fname in self._form_fields_whitelist:
+            _all_whitelisted[fname] = _all_fields[fname]
+        _all_fields = _all_whitelisted or _all_fields
         # update fields attributes
         self._form_update_fields_attributes(_all_fields)
         return _all_fields
@@ -53,12 +127,57 @@ class CMSForm(models.AbstractModel):
     def _form_update_fields_attributes(self, fields):
         """Manipulate fields attributes."""
         for fname in self._form_required_fields:
-            fields[fname]['required'] = True
+            # be defensive here since we can remove fields via blacklist
+            if fname in fields:
+                fields[fname]['required'] = True
 
     @property
     def form_file_fields(self):
         """File fields."""
-        return [f for f in self.form_fields() if f['type'] == 'binary']
+        return {
+            k: v for k, v in self.form_fields().iteritems()
+            if v['type'] == 'binary'
+        }
+
+    def _form_get_request_values(self):
+        return {k: v for k, v in self.request.form.iteritems()
+                if k not in ('csrf_token', )}
+
+    def form_load_defaults(self, main_object, request_values=None):
+        """Load default values.
+
+        Values lookup order:
+
+        1. `main_object` fields' values (if an existing main_object is passed)
+        2. request parameters (only parameters matching form fields names)
+        """
+        request_values = request_values or self._form_get_request_values()
+        defaults = request_values.copy()
+        form_fields = self.form_fields()
+        for fname, field in form_fields.iteritems():
+            value = None
+            # we could have form-only fields (like `custom` in test form below)
+            if main_object and fname in main_object:
+                value = main_object[fname]
+            # maybe a POST request with new values: override item value
+            # TODO: load particular fields too (file, image, etc)
+            value = request_values.get(fname, value)
+            # 1nd lookup for a default type handler
+            value_handler = self._form_loaders.get(field['type'], None)
+            # 2nd lookup for a specific type handler
+            value_handler = getattr(
+                self, '_form_load_' + field['type'], value_handler)
+            # 3rd lookup and override by named handler if any
+            value_handler = getattr(
+                self, '_form_load_' + fname, value_handler)
+            if value_handler:
+                value = value_handler(main_object, value, **request_values)
+            defaults[fname] = value
+        # add `has_*` flags for file fields
+        # so in templates we really know if a file field is valued.
+        for fname in self.form_file_fields.iterkeys():
+            defaults['has_' + fname] = bool(main_object[fname])
+        return defaults
 
 
 class TestPartnerForm(models.AbstractModel):
@@ -71,3 +190,6 @@ class TestPartnerForm(models.AbstractModel):
     _form_required_fields = ('name', 'country_id')
 
     custom = fields.Char()
+
+    def _form_load_custom(self, item, value, **req_values):
+        return req_values.get('custom', 'oh yeah!')
--- cms_form/tests/test_form.py
+++ cms_form/tests/test_form.py
@@ -3,6 +3,27 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp.tests.common import TransactionCase
+# from openerp.tests import HttpCase
+from openerp import http
+from werkzeug.wrappers import Request
+import mock
+from cStringIO import StringIO
+import urllib
+
+
+def fake_request(form_data=None, query_string=None,
+                 method='GET', content_type=None):
+    data = urllib.urlencode(form_data or {})
+    query_string = query_string or ''
+    content_type = content_type or 'application/x-www-form-urlencoded'
+    req = Request.from_values(
+        query_string=query_string,
+        content_length=len(data),
+        input_stream=StringIO(data),
+        content_type=content_type,
+        method=method)
+    req.session = mock.MagicMock()
+    return http.HttpRequest(req)
 
 
 class TestForm(TransactionCase):
@@ -13,6 +34,24 @@ class TestForm(TransactionCase):
     def setUp(self):
         super(TestForm, self).setUp()
 
+    def test_form_init(self):
+        request = fake_request()
+        form = self.env['cms.form']
+        form.form_init(request)
+        self.assertTrue(isinstance(form.request, Request))
+        self.assertTrue(isinstance(form.o_request, http.HttpRequest))
+
+    def test_form_init_overrides(self):
+        request = fake_request()
+        form = self.env['cms.form']
+        form.form_init(request,
+                       fields_whitelist=('name', ),
+                       fields_blacklist=('country_id', ),
+                       fields_attributes=('string', 'type', ))
+        self.assertEqual(form._form_fields_whitelist, ('name', ))
+        self.assertEqual(form._form_fields_blacklist, ('country_id', ))
+        self.assertEqual(form._form_fields_attributes, ('string', 'type', ))
+
     def test_fields(self):
         form = self.env['cms.form.partner_test']
         fields = form.form_fields()
@@ -21,9 +60,68 @@ class TestForm(TransactionCase):
         self.assertTrue('country_id' in fields.keys())
         self.assertTrue('custom' in fields.keys())
 
+        # whitelist
+        form = self.env['cms.form.partner_test']
+        form.form_init(fake_request(), fields_whitelist=('name', ))
+        fields = form.form_fields()
+        self.assertEqual(len(fields), 1)
+        self.assertTrue('name' in fields.keys())
+        self.assertTrue('country_id' not in fields.keys())
+        self.assertTrue('custom' not in fields.keys())
+
+        # blacklist
+        form = self.env['cms.form.partner_test']
+        form.form_init(fake_request(), fields_blacklist=('country_id', ))
+        fields = form.form_fields()
+        self.assertEqual(len(fields), 2)
+        self.assertTrue('name' in fields.keys())
+        self.assertTrue('country_id' not in fields.keys())
+        self.assertTrue('custom' in fields.keys())
+
     def test_fields_attributes(self):
         form = self.env['cms.form.partner_test']
         fields = form.form_fields()
         # fields from partner model
         self.assertTrue(fields['name']['required'])
         self.assertTrue(fields['country_id']['required'])
+
+    def test_load_defaults(self):
+        form = self.env['cms.form.partner_test']
+        request = fake_request()
+        form.form_init(request)
+
+        # create mode, no main_object
+        main_object = None
+        defaults = form.form_load_defaults(main_object)
+        expected = {
+            'name': None,
+            'country_id': None,
+            'custom': 'oh yeah!'
+        }
+        for k, v in expected.iteritems():
+            self.assertEqual(defaults[k], v)
+
+        # write mode, have main_object
+        main_object = self.env['res.partner'].new({})
+        main_object.name = 'John Wayne'
+        main_object.country_id = 5
+        defaults = form.form_load_defaults(main_object)
+        expected = {
+            'name': 'John Wayne',
+            'country_id': 5,
+            'custom': 'oh yeah!'
+        }
+        for k, v in expected.iteritems():
+            self.assertEqual(defaults[k], v)
+
+        # values from request
+        data = {
+            'name': 'Paul Newman',
+            'country_id': 7,
+            'custom': 'yay!'
+        }
+        request = fake_request(form_data=data)
+        form.form_init(request)
+        defaults = form.form_load_defaults(main_object)
+        for k, v in data.iteritems():
+            self.assertEqual(defaults[k], v)
